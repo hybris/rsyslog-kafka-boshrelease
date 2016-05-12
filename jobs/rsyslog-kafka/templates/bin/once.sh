a36db351f6aa11c7840609dd67064fd8adadabc0
Louis-Philippe Bedard <louis-philippe.bedard@sap.com>
2016-05-12 19:03:16 -0400

    gomkafka in blobs

--- jobs/rsyslog-kafka/templates/bin/once.sh
+++ jobs/rsyslog-kafka/templates/bin/once.sh
@@ -3,12 +3,10 @@
 # abort script on any command that exits with a non zero value
 set -e -x
 
-# download gomkafka module
-wget -q -O - https://github.com/hybris/gomkafka/releases/download/v0.1.0/gomkafka > /opt/gomkafka
+chown syslog:syslog /var/vcap/packages/gomkafka/var/vcap/bin/gomkafka
+chmod 0755 /var/vcap/packages/gomkafka/var/vcap/bin/gomkafka
 
-# Set permissions
-chown syslog:syslog /opt/gomkafka
-chmod 0755 /opt/gomkafka
+
 
 # move config file
 cp /var/vcap/jobs/rsyslog-kafka/etc/rsyslog.d/02-ygomkafka.conf /etc/rsyslog.d/02-ygomkafka.conf
