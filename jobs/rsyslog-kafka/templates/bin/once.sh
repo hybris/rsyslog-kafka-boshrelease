ed1004617a419890da90866b2e6e973dec57310e
Louis-Philippe Bedard <louis-philippe.bedard@sap.com>
2016-05-20 15:40:44 -0400

    add dynamic AWS AZ detection for dns brokers

--- jobs/rsyslog-kafka/templates/bin/once.sh
+++ jobs/rsyslog-kafka/templates/bin/once.sh
@@ -9,7 +9,15 @@ chmod 0755 /var/vcap/packages/gomkafka/var/vcap/bin/gomkafka
 
 
 # move config file
-cp /var/vcap/jobs/rsyslog-kafka/etc/rsyslog.d/02-ygomkafka.conf /etc/rsyslog.d/02-ygomkafka.conf
+# Check for [dynamic_availability_zone] placeholder
+AZ=""
+res=$(nc -z -w 3 169.254.169.254 80)
+if [ $? -eq 0 ];
+then
+  AZ=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone)
+fi;
+
+cat /var/vcap/jobs/rsyslog-kafka/etc/rsyslog.d/02-ygomkafka.conf | sed -e "s/\[dynamic_availability_zone\]/$AZ/g" > /etc/rsyslog.d/02-ygomkafka.conf
 
 # forward vcap logs
 if [ -f /etc/rsyslog.d/00-syslog_forwarder.conf ]; then
