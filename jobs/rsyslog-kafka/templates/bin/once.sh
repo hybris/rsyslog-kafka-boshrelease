20f174c07ff4abb588bf910d9902cde3434fd121
Louis-Philippe Bedard <louis-philippe.bedard@sap.com>
2016-06-23 14:17:53 -0400

    adding gomkafka/sauron agent jobs

--- jobs/rsyslog-kafka/templates/bin/once.sh
+++ jobs/rsyslog-kafka/templates/bin/once.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
 # abort script on any command that exits with a non zero value
-set -e -x
+set -x
 
-chown syslog:syslog /var/vcap/packages/gomkafka/var/vcap/bin/gomkafka
-chmod 0755 /var/vcap/packages/gomkafka/var/vcap/bin/gomkafka
-
-
-
-# move config file
 # Check for [dynamic_availability_zone] placeholder
 AZ=""
 res=$(nc -z -w 3 169.254.169.254 80)
@@ -17,6 +11,7 @@ then
   AZ=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone)
 fi;
 
+# move config file
 cat /var/vcap/jobs/rsyslog-kafka/etc/rsyslog.d/02-ygomkafka.conf | sed -e "s/\[dynamic_availability_zone\]/$AZ/g" > /etc/rsyslog.d/02-ygomkafka.conf
 
 # forward vcap logs
@@ -24,11 +19,5 @@ if [ -f /etc/rsyslog.d/00-syslog_forwarder.conf ]; then
   sed -i -e 's/^\(:programname, startswith, "vcap." ~\).*$/#\1/g' /etc/rsyslog.d/00-syslog_forwarder.conf
 fi;
 
-# restart rsyslog
-# service rsyslog restart
-
-# Create a cron to run the file generation
-(crontab -l | sed /generateCatchAll/d; echo "* * * * * /var/vcap/jobs/rsyslog-kafka/bin/generateCatchAll.sh") | sed /^$/d | crontab
-
-# Run it once now
-/var/vcap/jobs/rsyslog-kafka/bin/generateCatchAll.sh
+# Restart rsyslog to apply new changes
+service rsyslog restart
