5bb2fb890fc3d39528c21b997facb1970a22cb53
Louis-Philippe Bedard <louis-philippe.bedard@sap.com>
2016-06-23 15:08:25 -0400

    fix availability zone

--- jobs/rsyslog-kafka/templates/bin/once.sh
+++ jobs/rsyslog-kafka/templates/bin/once.sh
@@ -3,16 +3,8 @@
 # abort script on any command that exits with a non zero value
 set -x
 
-# Check for [dynamic_availability_zone] placeholder
-AZ=""
-res=$(nc -z -w 3 169.254.169.254 80)
-if [ $? -eq 0 ];
-then
-  AZ=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone)
-fi;
-
 # move config file
-cat /var/vcap/jobs/rsyslog-kafka/etc/rsyslog.d/02-ygomkafka.conf | sed -e "s/\[dynamic_availability_zone\]/$AZ/g" > /etc/rsyslog.d/02-ygomkafka.conf
+cp /var/vcap/jobs/rsyslog-kafka/etc/rsyslog.d/02-ygomkafka.conf /etc/rsyslog.d/02-ygomkafka.conf
 
 # forward vcap logs
 if [ -f /etc/rsyslog.d/00-syslog_forwarder.conf ]; then
